55f4bc8c52f0808a38ecaa9afa7e5a9d65049049
nmthong226 <nmthong226@gmail.com>
2024-10-06 15:43:22 +0700

    style: apply layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import About from './pages/About'
+import Layout from '@/layouts/PublicLayout.tsx'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </Router>
   )
