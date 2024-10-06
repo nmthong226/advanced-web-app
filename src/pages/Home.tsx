847b8753be3022a5b3c2ec5b799482a24570844d
nmthong226 <nmthong226@gmail.com>
2024-10-06 16:25:37 +0700

    chore: change sthing to test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex w-full h-full justify-center items-center text-2xl font-semibold">
-      Welcome, Homie. I miss you
+      Welcome, Homie. I miss you. Test test
     </div>
   )
 }
