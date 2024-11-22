be58c8490d4f7393d4d23d6660bd62796af9abb1
nmthong226 <nmthong226@gmail.com>
2024-11-22 18:48:46 +0700

    feat: add component's route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import About from './pages/About.tsx'
+import Dashboard from './pages/Dashboard/Dashboard.tsx'
 import Layout from '@/layouts/PublicLayout.tsx'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path="/about" element={<About />} />
         </Route>
       </Routes>
