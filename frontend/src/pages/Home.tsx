d44451c45dc0eb57dd133835f2806da7d2bcb7a6
nmthong226 <nmthong226@gmail.com>
2024-11-26 23:49:43 +0700

    chore: delete, rename & change dir

--- frontend/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="flex w-full h-full justify-center items-center text-2xl font-semibold">
-      Welcome, Homie. I miss you.
-    </div>
-  )
-}
-
-export default Home
