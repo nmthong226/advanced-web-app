e7993f95bbb67fb64f619acf8c906196f6967755
nmthong226 <nmthong226@gmail.com>
2024-12-05 12:22:07 +0700

    commit codes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const tailwindcssAnimate = require('tailwindcss-animate');
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
 	darkMode: ['class'],
