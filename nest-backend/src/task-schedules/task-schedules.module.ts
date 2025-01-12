43d84f54a21a2a94ae9c001b317c4de09f3c0dac
Dinh Huy <ldhuy21@clc.fitus.edu.vn>
2025-01-12 09:59:00 +0700

    delete folder be

--- nest-backend/src/task-schedules/task-schedules.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaskSchedulesController } from './task-schedules.controller';
-import { TaskSchedulesService } from './task-schedules.service';
-
-@Module({
-  controllers: [TaskSchedulesController],
-  providers: [TaskSchedulesService]
-})
-export class TaskSchedulesModule {}
